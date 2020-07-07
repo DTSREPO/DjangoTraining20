e82937fc6e33a9655b56f7d9c11d959a904e6088
unknown <rubelnl@gmail.com>
2020-07-07 15:32:21 +0600

    Rename project

--- my_project/manage.py
+++ my_project/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom_project_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project_app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- my_project/my_project_app/urls.py
+++ my_project/my_project_app/urls.py
@@ -1,4 +1,4 @@
-"""ecom_project_app URL Configuration
+"""my_project_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
